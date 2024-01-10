1c84dcf553e54f98647eba2c64723052426f5cdc
alegreco7 <alexgreco2002@gmail.com>
2024-01-10 16:12:35 +0100

    add stucture

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,41 @@
 <script>
-import AppHeader from "./components/AppHeader.vue";
-import AppCards from "./components/AppCards.vue";
+import AppHeader from './components/AppHeader.vue';
+import AppCards from './components/AppCards.vue';
+import { store } from './store';
+import axios from 'axios';
 export default {
-
   components: {
     AppHeader,
     AppCards
-  }
+  },
+  data() {
+    return {
+    }
+  },
+  mounted() {
+    this.getCard()
+  },
+  methods: {
+    getCard() {
+      axios.get(store.endpoint).then(response => (
+        store.cardsList = response.data.data
+      ))
+    }
+  },
 }
 </script>
+
 <template lang="">
-  <div>
-    
-  </div>
+  <AppHeader/>
+  <main>
+    <div class="container">
+      <div class="row">
+        <AppCards/>
+      </div>
+    </div>
+  </main>
 </template>
-<style lang="">
-  
+
+<style lang="scss" scoped>
+@use './styles/generals.scss';
 </style>
