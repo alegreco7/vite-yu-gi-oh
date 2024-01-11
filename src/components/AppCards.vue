cfbd133e8a6dfe5f63f60b8e88075eb94e7c796a
alegreco7 <alexgreco2002@gmail.com>
2024-01-11 18:36:05 +0100

    added link to store.js and structure for the cards

--- src/components/AppCards.vue
+++ src/components/AppCards.vue
@@ -1,13 +1,24 @@
 <script>
+import { store } from "../store";
 export default {
-
+    data() {
+        return {
+            store
+        }
+    }
 }
 </script>
 <template lang="">
-    <div>
-
+    <div class="col-6 col-md-4 col-lg-3"  v-for="(card, index) in store.cardsList" :key="index">
+        <div class="card">
+            <img :src="card.card_images[0].image_url" class="card-img-top" :alt="card.name">
+            <div class="card-body">
+              <h5 class="card-title">{{ card.name }}</h5>
+              <p class="card-text"> {{ card.archetype }}</p>
+            </div>
+          </div>
     </div>
 </template>
-<style lang="">
-    
+<style lang="scss" scoped>
+@use '../styles/generals.scss';
 </style>
